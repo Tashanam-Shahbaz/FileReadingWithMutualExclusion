8df03bc78a37d851fbd501031eb38318c9621206
Tashanam Shahbaz <64039299+Tashanam-Shahbaz@users.noreply.github.com>
2024-01-02 16:13:22 +0500

    Update Program.cs
    add mutual exclusion with multiple thread

--- FileReadingWithMutua Exclusion/Program.cs
+++ FileReadingWithMutua Exclusion/Program.cs
@@ -21,3 +21,46 @@ class Program
         Console.WriteLine("All processing complete.");
     }
 }
+
+
+class Program2
+{
+    static object fileLock = new object();
+
+    static void Main(string[] args)
+    {
+        string filePath = "C:\\Users\\tshahbaz\\source\\repos\\FileReadingWithMutualExclusion\\FileReadingWithMutua Exclusion\\TestFile.txt";
+        int numberOfThreads = 4;
+
+        lock (fileLock)
+        {
+
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.WriteLine($"Main Thread  writing to the file at {DateTime.Now}");
+            }
+
+            Console.WriteLine($" Main Thread  completed writing.");
+        }
+
+        Parallel.For(0, numberOfThreads, i =>
+        {
+           
+            lock (fileLock) 
+            {
+               
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.WriteLine($"Thread {i + 1} writing to the file at {DateTime.Now}");
+                }
+
+                Console.WriteLine($"Thread {i + 1} completed writing.");
+            }
+
+          
+            Thread.Sleep(1000);
+        });
+
+        Console.WriteLine("All processes finished executing.");
+    }
+}
