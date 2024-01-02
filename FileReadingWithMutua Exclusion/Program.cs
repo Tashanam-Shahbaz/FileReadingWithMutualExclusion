ac511cabed078ede61a54b910f374e45ee2f958a
Tashanam Shahbaz <tshahbaz@trafix.com>
2024-01-02 16:11:44 +0500

    Add project files.

---
+++ FileReadingWithMutua Exclusion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+// Multi-threading
+class Program
+{
+
+    static void Main1(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Parallel.ForEach(numbers, i =>
+        {
+            Console.WriteLine($"Processing number {i} on thrad {Task.CurrentId}");
+
+        });
+
+        Console.WriteLine("All processing complete.");
+    }
+}
